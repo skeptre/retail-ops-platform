eb82c5d34590122176be12c3f5e1aec669216fd3
skeps <aliskepss@gmail.com>
2026-06-08 23:15:55 +0100

    Refactor connection.py

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,11 +1,11 @@
 import os
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
+import sqlalchemy
+import dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
 
 def get_engine():
     url = os.getenv('DATABASE_URL')
     if not url:
-        raise ValueError('DATABASE_URL not set in environment')
-    return create_engine(url)
+        raise ValueError("DATABASE_URL not set in environment")
+    return sqlalchemy.create_engine(url)
